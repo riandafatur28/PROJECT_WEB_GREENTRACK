bd04f61784625bb7be598c19dd5a1084646eb84b
Rianda <e41231605@student.polije.ac.id>
2025-02-28 16:31:45 +0700

    memperbarui vite js

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,29 @@
-<<<<<<< HEAD
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "path";
-
-export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "resources/js"),
-        },
-    },
-    build: {
-        outDir: "public/build",
-    },
-    server: {
-        proxy: {
-            "/app": "http://localhost",
-        },
-    },
-=======
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
     ],
->>>>>>> 850b15a7fa3f55b7db7e78908d138f1ac6af5019
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
+    build: {
+        outDir: 'public/build',
+    },
+    server: {
+        proxy: {
+            '/app': 'http://localhost',
+        },
+    },
 });
