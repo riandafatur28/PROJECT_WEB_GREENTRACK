28f43631caa0652e5dad1fbd411c3f2fd075f290
Rianda <e41231605@student.polije.ac.id>
2025-02-28 16:18:45 +0700

    mengedit vite.config.js #1

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "resources/js"),
+        },
+    },
+    build: {
+        outDir: "public/build",
+    },
+    server: {
+        proxy: {
+            "/app": "http://localhost",
+        },
+    },
 });
