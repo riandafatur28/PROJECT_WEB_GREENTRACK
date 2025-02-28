b9cd4a653e50147ba17180081db8f4245b6dc62d
Rianda <e41231605@student.polije.ac.id>
2025-02-28 16:20:18 +0700

    Menambahkan Route menuju login.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('layouts.dashboard');
+});
+
+Route::get('/login', function () {
+    return view('layouts.login'); // This assumes your Blade view is located in resources/views/login.blade.php
+});
+
+Route::post('/login', [LoginController::class, 'login']);
