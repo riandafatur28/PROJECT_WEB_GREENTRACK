eb13e39d529e249e46a799cf1496fcf709a9cacc
Rianda <e41231605@student.polije.ac.id>
2025-02-28 16:24:05 +0700

    menambahkan font inter

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | GreenTrack</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
 </head>
 
 <body class="flex h-screen">
