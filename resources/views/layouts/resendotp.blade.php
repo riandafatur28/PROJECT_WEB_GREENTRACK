b8348cbfca2b04f7cb6dabcd6370fc7a1112cf2a
Rianda <e41231605@student.polije.ac.id>
2025-02-28 23:21:57 +0700

    mengupdate tampilan resendotp #2

--- resources/views/layouts/resendotp.blade.php
+++ resources/views/layouts/resendotp.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Verifikasi Kode OTP | GreenTrack</title>
     <script src="https://cdn.tailwindcss.com"></script>
