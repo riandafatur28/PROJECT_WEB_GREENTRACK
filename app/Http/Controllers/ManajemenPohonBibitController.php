6e3208fc3b06c034c120f3216d0d9de0f4bdda0d
Rianda <e41231605@student.polije.ac.id>
2025-03-11 10:34:30 +0700

    update controller manajemenpohotbibit

--- app/Http/Controllers/ManajemenPohonBibitController.php
+++ app/Http/Controllers/ManajemenPohonBibitController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -17,10 +18,10 @@ class ManajemenPohonBibitController extends Controller
 
         $bibit = [
             ['id' => '001', 'nama' => 'Jati', 'jenis' => 'bibit', 'usia' => '14 Hari', 'jumlah' => '100', 'lokasi' => 'Bagor', 'status' => 'Penyemaian'],
-            ['id' => '002', 'nama' => 'Mahoni','jenis' => 'bibit', 'usia' => '30 Hari', 'jumlah' => '100', 'lokasi' => 'Rejoso', 'status' => 'Penyemaian'],
-            ['id' => '003', 'nama' => 'Jati', 'jenis' => 'bibit','usia' => '3 Hari', 'jumlah' => '100', 'lokasi' => 'Bagor', 'status' => 'Penyemaian'],
+            ['id' => '002', 'nama' => 'Mahoni', 'jenis' => 'bibit', 'usia' => '30 Hari', 'jumlah' => '100', 'lokasi' => 'Rejoso', 'status' => 'Penyemaian'],
+            ['id' => '003', 'nama' => 'Jati', 'jenis' => 'bibit', 'usia' => '3 Hari', 'jumlah' => '100', 'lokasi' => 'Bagor', 'status' => 'Penyemaian'],
             ['id' => '004', 'nama' => 'Mahoni', 'jenis' => 'bibit', 'usia' => '5 Hari', 'jumlah' => '100', 'lokasi' => 'Bagor', 'status' => 'Penyemaian'],
-            ['id' => '005', 'nama' => 'Jati', 'jenis' => 'bibit','usia' => '6 Hari', 'jumlah' => '100', 'lokasi' => 'Rejoso', 'status' => 'Penyemaian'],
+            ['id' => '005', 'nama' => 'Jati', 'jenis' => 'bibit', 'usia' => '6 Hari', 'jumlah' => '100', 'lokasi' => 'Rejoso', 'status' => 'Penyemaian'],
         ];
 
         return view('layouts.manajemenkayubibit', compact('kayu', 'bibit'));
