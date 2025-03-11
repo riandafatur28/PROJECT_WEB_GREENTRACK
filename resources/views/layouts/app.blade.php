b22cad8689b511fbdb035efb974ecc164c3f7992
Rianda <e41231605@student.polije.ac.id>
2025-03-11 10:36:50 +0700

    update app blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'GreenTrack')</title>
     <script src="https://cdn.tailwindcss.com"></script>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
+    </style>
+
+
 </head>
 
 <body class="bg-gray-100">
