9a2342c56bc1940d83975a4897e22fa37c61b3be
Rianda <e41231605@student.polije.ac.id>
2025-03-14 13:07:10 +0700

    mengupdate app blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'GreenTrack')</title>
+    <title>GreenTrack | @yield('title', 'Dashboard')</title>
     <script src="https://cdn.tailwindcss.com"></script>
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
