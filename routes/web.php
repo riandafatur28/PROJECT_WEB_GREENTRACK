eebb4a1eaecacc47bb7e835dbe7fd7dc3800d5c6
Rianda <e41231605@student.polije.ac.id>
2025-04-13 06:34:17 +0700

    update routes web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ManajemenPenggunaController;
 use App\Http\Controllers\ManajemenPohonBibitController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Config;
 
 Route::get('/', function () {
     return view('welcome');
@@ -30,14 +31,22 @@ Route::get('/profile', function () {
     return view('layouts.profile');
 })->name('profile');
 
+Route::get('/test-firebase', function () {
+    $firebaseCredentials = Config::get('firebase.credentials');
+
+    if (file_exists($firebaseCredentials)) {
+        return 'Firebase credentials file found at: ' . $firebaseCredentials;
+    }
+
+    return 'Firebase credentials file not found!';
+}); // <-- Menambahkan kurung tutup yang hilang di sini
+
 Route::get('/manajemen-kayu-bibit', [ManajemenPohonBibitController::class, 'index'])
     ->name('manajemenkayubibit');
 
-    Route::get('/history-scan-barcode', [HistoryBarcodeController::class, 'index'])
+Route::get('/history-scan-barcode', [HistoryBarcodeController::class, 'index'])
     ->name('historyscanbarcode');
-
 
 Route::get('/manajemen-pengguna', [ManajemenPenggunaController::class, 'index'])->name('manajemenpengguna');
 
 Route::post('/login', [LoginController::class, 'login']);
-
